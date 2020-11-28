bbf6f43c617977fae93bcd0b9deb86c28c20f674
tejas128 <ladtejas128@gmail.com>
2020-11-28 16:19:58 +0530

    Updates

---
+++ precache-manifest.1c45dd4a843f0181555c598037a6e362.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4caabe8de56f1c537d6cf0a949a209c",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "2b1183ad0cf687131dca",
+    "url": "/covidtracker/static/css/main.73810fe4.chunk.css"
+  },
+  {
+    "revision": "4ef1146462f5922a4c51",
+    "url": "/covidtracker/static/js/2.cd8d8929.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covidtracker/static/js/2.cd8d8929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1183ad0cf687131dca",
+    "url": "/covidtracker/static/js/main.0c388ddd.chunk.js"
+  },
+  {
+    "revision": "c10044dbd4bbf8c06e07",
+    "url": "/covidtracker/static/js/runtime-main.a25154e9.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/covidtracker/static/media/covid19_logo.6ce9074d.png"
+  }
+]);
